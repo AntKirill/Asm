9fafce5ad536fb178dcfc1beebe47aceb34000c8
kirant <kirant9797@gmail.com>
2017-10-13 22:36:15 +0300

    removed extra at world count

--- word_counter/world_count.cpp
+++ word_counter/world_count.cpp
@@ -22,36 +22,30 @@ size_t word_count_fast(const char *str, size_t size) {
     static const __m128i all_spaces = _mm_set1_epi8(' ');
     static const __m128i all_zeros = _mm_set1_epi8(0);
     static const __m128i all_ff = _mm_set1_epi8(static_cast<char>(0xff));
-    static const __m128i highest_ff = _mm_set_epi8(static_cast<char>(0xff), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
     if (size < 32) return word_count_slow(str, size);
 
     const char *p = str;
     bool prv_space = true;
     size_t cnt = 0;
+
     //first easy count of not aligned
     for (; reinterpret_cast<size_t>(p) % 16 != 0; p++) {
         if (prv_space && *p != ' ') cnt++;
         prv_space = (*p == ' ');
     }
-
     size_t rest = (static_cast<size_t>(str + size - p)) % 16;
 
     __m128i prv_store = prv_space ? all_ff : all_zeros;
     for (; p != str + size - rest; p += 16) {
         __m128i store = _mm_cmpeq_epi8(_mm_load_si128(reinterpret_cast<const __m128i *>(p)), all_spaces);
-        __m128i word_mask = _mm_and_si128(_mm_xor_si128(all_ff, store),
-                                          _mm_alignr_epi8(store, prv_store, 15));
+        __m128i word_mask = _mm_and_si128(_mm_xor_si128(all_ff, store), _mm_alignr_epi8(store, prv_store, 15));
 
         cnt += popcnt128(word_mask) / 8;
         prv_store = store;
     }
 
-    __m128i a = highest_ff;
-    __m128i b = _mm_and_si128(prv_store, a);
-    int k = _mm_movemask_epi8(b);
-    prv_space = (k != 0);
-
+    prv_space = (*(p - 1) == ' ');
     //second easy count of not aligned
     for (; p != str + size; p++) {
         if (prv_space && *p != ' ') cnt++;
